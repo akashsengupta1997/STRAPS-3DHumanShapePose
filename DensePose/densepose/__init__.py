f75e934d4e48eca0d4423a4d95674caff235c764
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-05-22 17:48:06 +0100

    clean

--- DensePose/densepose/__init__.py
+++ DensePose/densepose/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from . import dataset  # just to register data
+# from . import dataset  # just to register data
 from .config import add_densepose_config
 from .dataset_mapper import DatasetMapper
 from .densepose_head import ROI_DENSEPOSE_HEAD_REGISTRY
