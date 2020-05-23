7d6321c5bc904b3aa45ebcf3b2adc337657baeb8
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-05-23 14:19:28 +0100

    Update run_predict.py

--- run_predict.py
+++ run_predict.py
@@ -9,4 +9,4 @@ from predict.predict import predict_3D
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 predict_3D("/data/cvfs/as2562/datasets/toy_datasets/testing/cropped_frames", None,
-           silhouettes_from='densepose')
+           silhouettes_from='pointrend')
