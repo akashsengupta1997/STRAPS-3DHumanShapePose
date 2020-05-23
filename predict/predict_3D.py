4b0305cacd03a8f3f73c618bf85c66358b0ce6df
Akash Sengupta <akash.sengupta1997@gmail.com>
2020-05-23 16:56:34 +0100

    added 3d prediction script

--- predict/predict_3D.py
+++ predict/predict_3D.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import numpy as np
+import torch
+from smplx.lbs import batch_rodrigues
 
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
@@ -15,16 +17,18 @@ from predict.predict_joints2D import predict_joints2D
 from predict.predict_silhouette_pointrend import predict_silhouette_pointrend
 from predict.predict_densepose import predict_densepose
 
+from models.smpl_official import SMPL
+from renderers.weak_perspective_pyrender_renderer import Renderer
+
 from utils.image_utils import pad_to_square
 from utils.label_conversions import convert_multiclass_to_binary_labels, \
     convert_2Djoints_to_gaussian_heatmaps
+from utils.rigid_transform_utils import rot6d_to_rotmat
 
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-# TODO pre-processing step (pad to square)
-# TODO 3D predict
 
 def setup_detectron2_predictors(silhouettes_from='densepose'):
     # Keypoint-RCNN
@@ -42,7 +46,7 @@ def setup_detectron2_predictors(silhouettes_from='densepose'):
         pointrend_cfg = get_cfg()
         add_pointrend_config(pointrend_cfg)
         pointrend_cfg.merge_from_file(pointrend_config_file)
-        pointrend_cfg.MODEL.WEIGHTS = "PointRend/checkpoints/pointrend_rcnn_R_50_fpn.pkl"
+        pointrend_cfg.MODEL.WEIGHTS = "checkpoints/pointrend_rcnn_R_50_fpn.pkl"
         pointrend_cfg.freeze()
         silhouette_predictor = DefaultPredictor(pointrend_cfg)
     elif silhouettes_from == 'densepose':
@@ -51,7 +55,7 @@ def setup_detectron2_predictors(silhouettes_from='densepose'):
         densepose_cfg = get_cfg()
         add_densepose_config(densepose_cfg)
         densepose_cfg.merge_from_file(densepose_config_file)
-        densepose_cfg.MODEL.WEIGHTS = "DensePose/checkpoints/densepose_rcnn_R_101_fpn_s1x.pkl"
+        densepose_cfg.MODEL.WEIGHTS = "checkpoints/densepose_rcnn_R_101_fpn_s1x.pkl"
         densepose_cfg.freeze()
         silhouette_predictor = DefaultPredictor(densepose_cfg)
 
@@ -77,11 +81,18 @@ def create_proxy_representation(silhouette,
 
 def predict_3D(input,
                regressor,
+               device,
                silhouettes_from='densepose',
                proxy_rep_input_wh=512):
 
-    # Set-up proxy representation predictors
+    # Set-up proxy representation predictors.
     joints2D_predictor, silhouette_predictor = setup_detectron2_predictors(silhouettes_from=silhouettes_from)
+
+    # Set-up SMPL model.
+    smpl = SMPL(config.SMPL_MODEL_DIR, batch_size=1)
+
+    # Set-up renderer for visualisation.
+    wp_renderer = Renderer(resolution=(proxy_rep_input_wh, proxy_rep_input_wh))
 
     if os.path.isdir(input):
         image_fnames = [f for f in sorted(os.listdir(input)) if f.endswith('.png') or
@@ -107,16 +118,48 @@ def predict_3D(input,
                                                     in_wh=proxy_rep_input_wh,
                                                     out_wh=config.REGRESSOR_IMG_WH)
             proxy_rep = proxy_rep[None, :, :, :]  # add batch dimension
+            proxy_rep = torch.from_numpy(proxy_rep).float().to(device)
 
-            # TODO predict 3D
+            # Predict 3D
+            pred_cam_wp, pred_pose, pred_shape = regressor(proxy_rep)
+            # Convert pred pose to rotation matrices
+            if pred_pose.shape[-1] == 24 * 3:
+                pred_pose_rotmats = batch_rodrigues(pred_pose.contiguous().view(-1, 3))
+                pred_pose_rotmats = pred_pose_rotmats.view(-1, 24, 3, 3)
+            elif pred_pose.shape[-1] == 24 * 6:
+                pred_pose_rotmats = rot6d_to_rotmat(pred_pose.contiguous()).view(-1, 24, 3, 3)
+
+            pred_smpl_output = smpl(body_pose=pred_pose_rotmats[:, 1:],
+                                    global_orient=pred_pose_rotmats[:, 0].unsqueeze(1),
+                                    betas=pred_shape,
+                                    pose2rot=False)
+            pred_vertices = pred_smpl_output.vertices
+
+            pred_reposed_smpl_output = smpl(betas=pred_shape)
+            pred_reposed_vertices = pred_reposed_smpl_output.vertices
+
+            # Numpy-fying
+            pred_vertices = pred_vertices.cpu().detach().numpy()[0]
+            pred_reposed_vertices = pred_reposed_vertices.cpu().detach().numpy()[0]
+            pred_cam_wp = pred_cam_wp.cpu().detach().numpy()[0]
+
+            # TODO comment this out with newer models trained with translation before scaling
+            pred_cam_wp[1:] = pred_cam_wp[1:] / pred_cam_wp[0]  # Need to do this division because of different cam translation convention (described in my research diary)
+            rend_img = wp_renderer.render(verts=pred_vertices, cam=pred_cam_wp, img=image)
+            rend_reposed_img = wp_renderer.render(verts=pred_reposed_vertices,
+                                                  cam=[0., 0., proxy_rep_input_wh/10])
 
             plt.figure()
-            plt.subplot(221)
+            plt.subplot(231)
             plt.imshow(joints2D_vis)
-            plt.subplot(222)
+            plt.subplot(232)
             plt.imshow(silhouette)
-            plt.subplot(223)
+            plt.subplot(233)
             plt.imshow(silhouette_vis)
-            plt.subplot(224)
+            plt.subplot(234)
             plt.imshow(np.sum(proxy_rep[0], axis=0))
+            plt.subplot(235)
+            plt.imshow(rend_img)
+            plt.subplot(236)
+            plt.imshow(rend_reposed_img)
             plt.show()
--- run_predict.py
+++ run_predict.py
@@ -1,9 +1,30 @@
 import os
-import cv2
+import argparse
+import torch
 
+from models.regressor import SingleInputRegressor
 from predict.predict_3D import predict_3D
 
+def main(input_path, checkpoint_path, device, silhouettes_from):
+    regressor = SingleInputRegressor(resnet_in_channels=18,
+                                     resnet_layers=18,
+                                     ief_iters=3)
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-predict_3D("demo/images", None, silhouettes_from='pointrend')
+    print("\nModel Loaded. ",'Weights from:', checkpoint_path)
+    regressor.to(device)
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    regressor.load_state_dict(checkpoint['best_model_state_dict'])
+
+    predict_3D(input_path, regressor, device, silhouettes_from=silhouettes_from)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, help='Path to input image/folder of images.')
+    parser.add_argument('--checkpoint', type=str, help='Path to model checkpoint')
+    parser.add_argument('--silh_from', choices=['densepose', 'pointrend'])
+    args = parser.parse_args()
+
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
+    main(args.input, args.checkpoint, device, args.silh_from)
